f1e03ed685a2c6420e818912b9a6b0dc626bb287
braggdl <braggdl@udmercy.edu>
2026-07-09 15:35:52 -0400

    Add Firebase configuration to firebase-config.js
    Added Firebase project configuration values.

--- services/firebase-config.js
+++ services/firebase-config.js
@@ -4,13 +4,15 @@
   Do NOT commit real secrets to public repositories.
 */
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // apiKey: "YOUR_API_KEY",
-  // authDomain: "YOUR_AUTH_DOMAIN",
-  // projectId: "YOUR_PROJECT_ID",
-  // storageBucket: "YOUR_STORAGE_BUCKET",
-  // messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  // appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBXPP4lfYO_OSYiv4DdnsoiYEZTl7d0t4I",
+  authDomain: "playground-pulse-8e2c4.firebaseapp.com",
+  projectId: "playground-pulse-8e2c4",
+  storageBucket: "playground-pulse-8e2c4.firebasestorage.app",
+  messagingSenderId: "1077539477518",
+  appId: "1:1077539477518:web:0500c43a256019e685845f",
+  measurementId: "G-TWZZQYR7W3"
 };
 
 export { firebaseConfig };
